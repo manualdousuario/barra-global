0a6bd505747cf7b2049ae53fa70302834db4f683
Renan Altendorf <altendorfme@users.noreply.github.com>
2025-10-26 22:47:52 -0300

    barra global para aplicação via nginx bar.js

---
+++ bar.js
@@ -0,0 +1,42 @@
+function eTttKfUa6c94R() {
+    const html = `
+  <style nonce="eTttKfUa6c94R">
+    :root{--altura:36px}
+    body{padding-top:0 !important;margin:0 0 0 auto !important}
+    .top-bar-eTttKfUa6c94R{list-style:none;background:hsl(0, 0%, 97%);border-bottom:1px solid hsl(0, 0%, 87%);font-size:14px;font-family:ui-monospace, monospace;display:flex;align-items:center;justify-content:space-around;z-index:99999;margin:0;padding:0;height:var(--altura);white-space:nowrap;overflow-x:auto;overflow-y:hidden;scrollbar-width:none}
+    .top-bar-eTttKfUa6c94R::-webkit-scrollbar{display:none}
+    .top-bar-eTttKfUa6c94R *{margin:0;padding:0}
+    .top-bar-eTttKfUa6c94R li{margin:0}
+    .top-bar-eTttKfUa6c94R ul li a{padding:10px 6px}
+    .top-bar-eTttKfUa6c94R a{flex-shrink:0;font-weight:400}
+    .top-bar-eTttKfUa6c94R a,.top-bar-eTttKfUa6c94R a:visited{color:hsl(229, 100%, 39%);text-decoration:underline}
+    .top-bar-eTttKfUa6c94R a:hover{color:hsl(240, 63%, 27%)}
+    @media (max-width: 728px){.top-bar-eTttKfUa6c94R{justify-content:start;column-gap:1rem;padding-inline:1rem}}
+    @media (prefers-color-scheme: dark){
+      .top-bar-eTttKfUa6c94R{background-color:hsl(0, 0%, 18%);box-shadow:none}
+      .top-bar-eTttKfUa6c94R a,.top-bar-eTttKfUa6c94R a:visited{color:hsl(224, 100%, 83%)}
+      .top-bar-eTttKfUa6c94R a:hover{color:hsl(224, 100%, 71%)}
+    }
+    header.container{margin-top:1rem}
+    .side-nav.fixed{padding-top:var(--altura)}
+  </style>
+  <ul class="top-bar-eTttKfUa6c94R">
+    <li><a href="https://manualdousuario.net/">Blog</a></li>
+    <li><a href="https://manualdousuario.net/orbita/">Órbita</a></li>
+    <li><a href="https://manualdousuario.net/apoie/"><strong>Assine</strong></a></li>
+    <li><a href="https://pcdomanual.com/">PC do Manual</a></li>
+    <li><a href="https://manualdousuario.net/podcast/">Podcasts</a></li>
+    <li><a href="https://manualdousuario.net/loja/">Lojinha</a></li>
+    <li><a href="https://manualdousuario.net/newsletters-brasileiras/">Newsletters BR</a></li>
+    <li><a href="https://lerama.pcdomanual.com">Lerama</a></li>
+  </ul>
+  `;
+
+    document.body.insertAdjacentHTML('afterbegin', html);
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', eTttKfUa6c94R);
+} else {
+    eTttKfUa6c94R();
+}
